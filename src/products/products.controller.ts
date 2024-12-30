4d03e09e1df89b09a8ae6b73a3b3e45b26aa0c2c
Elias Adad <eliasdev97@gmail.com>
2024-12-30 20:27:33 -0300

    adds auth guard to products endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post('add')
+  @UseGuards(AuthGuard)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -23,11 +25,13 @@ export class ProductsController {
   }
 
   @Put('update/:id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
+  @Delete('delete/:id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
