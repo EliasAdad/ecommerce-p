b3f8bcc4e178f3fca999bd72f23cd0b1d665982e
Elias Adad <eliasdev97@gmail.com>
2024-12-30 22:01:56 -0300

    creates a configuration object for connection with the db using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
-
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './.env.development'
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('DB_NAME'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        synchronize: true,
+        logging: true
+      }),
+    }),
     UsersModule, ProductsModule, AuthModule],
   controllers: [],
   providers: [],
